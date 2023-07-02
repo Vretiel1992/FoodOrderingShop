f0e22cfa905462246e1fbf4ec1b8fe81c71e4ada
Антон Денисюк <Vretiel@icloud.com>
2023-07-02 13:17:15 +0300

    initial commit

---
+++ FoodOrderingShop/PresentationLayer/MainModule/View/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  FoodOrderingShop
+//
+//  Created by Антон Денисюк on 02.07.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemIndigo
+    }
+}
